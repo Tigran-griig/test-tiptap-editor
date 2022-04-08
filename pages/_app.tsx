b16749d0ec5824e5daf8f9960eb2f28669a05310
Tigran-griig <tigrangrigoryan.it@gmail.com>
2022-04-08 22:59:24 +0400

    small fix app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,25 @@ import "reflect-metadata";
 import '../components/TextEditor/textEditor.scss';
 import '../components/TextStyleToolbar/TextEditor.scss'
 import "../styles/global.scss";
-import type { AppProps } from "next/app";
+import "styles/globals.css";
+
+import {NextPage} from "next";
+import {AppProps} from "next/app";
+import {ReactElement, ReactNode} from "react";
 
 
+type NextPageWithLayout = NextPage & {
+    getLayout?: (page: ReactElement) => ReactNode;
+};
 
-export default function MyApp({
-  Component,
-  pageProps,
-}: AppProps) {
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout;
+};
 
-  return (
-      <>
-        <Component {...pageProps} />
-      </>
-  );
-}
+const MyApp = ({Component, pageProps}: AppPropsWithLayout) => {
+    const getLayout = Component.getLayout ?? ((page: ReactElement) => page);
+
+    return ({getLayout(<Component {...pageProps} />)});
+};
+
+export default MyApp;
