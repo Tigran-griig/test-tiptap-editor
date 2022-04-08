f9964a2a3a2381e81d318597513eb83a9513e3f9
Tigran-griig <tigrangrigoryan.it@gmail.com>
2022-04-08 23:19:21 +0400

    update deps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,15 @@
-import {ReactElement, ReactNode} from "react";
 import "../styles/global.scss";
 import "reflect-metadata";
 import '../components/TextEditor/textEditor.scss';
 import '../components/TextStyleToolbar/TextEditor.scss'
 import "../styles/global.scss";
-import "styles/globals.css";
 
-import {NextPage} from "next";
-import {AppProps} from "next/app";
-
-
-type NextPageWithLayout = NextPage & {
-    getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-    Component: NextPageWithLayout;
-};
-
-const MyApp = ({Component, pageProps}: AppPropsWithLayout) => {
-    const getLayout = Component.getLayout ?? ((page: ReactElement) => page);
-
+const MyApp = ({Component, pageProps:{...pageProps}}) => {
     return (
-        {getLayout(<Component {...pageProps} />)}
-);
+        <div>
+            <Component {...pageProps} />
+        </div>
+    );
 };
 
 export default MyApp;
