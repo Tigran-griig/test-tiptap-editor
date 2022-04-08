01076b8c0aa8dd9a3a12a1c723dfcc65513d3bf1
Tigran-griig <tigrangrigoryan.it@gmail.com>
2022-04-08 22:52:27 +0400

    add fragment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,17 @@ import '../components/TextEditor/textEditor.scss';
 import '../components/TextStyleToolbar/TextEditor.scss'
 import "../styles/global.scss";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
 
 
 
 export default function MyApp({
   Component,
-  pageProps: { session, ...pageProps },
+  pageProps: {...pageProps },
 }: AppProps) {
-  const router = useRouter();
+
   return (
+      <>
         <Component {...pageProps} />
+      </>
   );
 }
